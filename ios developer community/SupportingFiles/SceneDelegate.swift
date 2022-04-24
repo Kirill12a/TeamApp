5ac6bba4abb3bd98417a2634900c70090622c2ca
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-04-24 16:32:58 +0300

    Добавил скелет mvc

---
+++ ios developer community/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  ios developer community
+//
+//  Created by Kirill Drozdov on 24.04.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+    window?.windowScene = windowScene
+    window?.rootViewController = OurLinksViewController()
+    window?.makeKeyAndVisible()
+  }
+
+  func sceneDidDisconnect(_ scene: UIScene) {
+  }
+
+  func sceneDidBecomeActive(_ scene: UIScene) {
+  }
+
+  func sceneWillResignActive(_ scene: UIScene) {
+  }
+
+  func sceneWillEnterForeground(_ scene: UIScene) {
+  }
+
+  func sceneDidEnterBackground(_ scene: UIScene) {
+  }
+
+
+}
+
