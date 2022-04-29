5db38ec5ae7b2ce35a8f933ec9b74eb13a737fb3
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-04-29 16:59:01 +0300

    юю

--- ios developer community/Controller/TabBar/TabBar.swift
+++ ios developer community/Controller/TabBar/TabBar.swift
@@ -9,29 +9,29 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    let newsVC      =    OurLinksViewController()
-    let libraryVC   =   MaterialsViewController()
+        let newsVC      =    OurLinksViewController()
+        let libraryVC   =   MaterialsViewController()
 
-    viewControllers = [
-      setUpNavigationController(title: "Новости", image: UIImage(systemName: "newspaper"), rootViewController: newsVC),
-      setUpNavigationController(title: "Библиотека", image: UIImage(systemName: "book"), rootViewController: libraryVC)
-    ]
+        viewControllers = [
+            setUpNavigationController(title: "Новости", image: UIImage(systemName: "newspaper"), rootViewController: newsVC),
+            setUpNavigationController(title: "Библиотека", image: UIImage(systemName: "book"), rootViewController: libraryVC)
+        ]
 
-      tabBar.backgroundColor = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 0.94)
-      tabBar.unselectedItemTintColor = .gray
-      tabBar.tintColor = .red
+        tabBar.backgroundColor = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 0.94)
+        tabBar.unselectedItemTintColor = .gray
+        tabBar.tintColor = .red
 
 
-  }
+    }
 
-  private func setUpNavigationController(title: String, image: UIImage?, rootViewController: UIViewController) -> UIViewController {
-    let navigationViewController = UINavigationController(rootViewController: rootViewController)
-    navigationViewController.tabBarItem.image = image
-    navigationViewController.tabBarItem.title = title
+    private func setUpNavigationController(title: String, image: UIImage?, rootViewController: UIViewController) -> UIViewController {
+        let navigationViewController = UINavigationController(rootViewController: rootViewController)
+        navigationViewController.tabBarItem.image = image
+        navigationViewController.tabBarItem.title = title
 
-    return navigationViewController
-  }
+        return navigationViewController
+    }
 }
